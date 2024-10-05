c39d4824ef6b116f45e12e58aec7d8b90f2bca82
Ilovemovies-11 <sahadevapranav11@gmail.com>
2024-10-05 14:35:50 +0530

    Add countries API controller

---
+++ Fantasy/Fantasy.Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Fantasy.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Fantasy.Backend.Data;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+      public Microsoft.EntityFrameworkCore.DbSet<Country> Countries { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
--- Fantasy/fantasy.Frontend/Program.cs
+++ Fantasy/fantasy.Frontend/Program.cs
@@ -1,4 +1,4 @@
-using fantasy.Frontend;
+using Fantasy.Frontend;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
